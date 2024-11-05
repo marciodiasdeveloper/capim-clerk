ee1b388bfc5b6c7974ea7e95292b3e23b3585efc
marcio dias <marcio@marciodias.me>
2024-11-05 16:32:14 -0300

    feat: initial commit

--- src/domain/use-cases/tasks/delete.ts
+++ src/domain/use-cases/tasks/delete.ts
@@ -1,12 +1,11 @@
 import { TasksDelete } from '@/domain/contracts/gateways'
-import { getAuth } from 'firebase/auth'
 
-type Setup = (firebase: any) => TasksDeleteUseCase
+type Setup = () => TasksDeleteUseCase
 type Input = TasksDelete.Input
 type Output = TasksDelete.Output
 
 export type TasksDeleteUseCase = (input: Input) => Promise<Output>
 
-export const setupTasksDeleteUseCase: Setup = ({ firebase }) => async input => {
+export const setupTasksDeleteUseCase: Setup = () => async input => {
   return { success: true }
 }
--- src/infra/gateways/authenticate-middleware.ts
+++ src/infra/gateways/authenticate-middleware.ts
@@ -1,32 +1,13 @@
 import { AuthorizationTokenValidate } from '@/domain/contracts/gateways'
 
 // import { ValidateUser } from 'library-auth'
-import { env } from '@/main/config/env'
-import { FirebaseApp, initializeApp } from 'firebase/app'
-import { getAuth, signInWithCustomToken } from 'firebase/auth'
 
 export class AuthenticateMiddleware implements AuthorizationTokenValidate {
   async authorizeToken (input: AuthorizationTokenValidate.Input): Promise<AuthorizationTokenValidate.Output> {
-    const firebaseConfig = {
-      apiKey: env.firebase.apiKey,
-      authDomain: env.firebase.authDomain,
-      projectId: env.firebase.projectId,
-      storageBucket: env.firebase.storageBucket,
-      messagingSenderId: env.firebase.messagingSenderId,
-      appId: env.firebase.appId
-    }
-
-    const firebase: FirebaseApp = initializeApp(firebaseConfig)
-
-    const auth = getAuth(firebase)
-
     let result
     try {
-      console.log('AuthenticateMiddleware input', input)
-      const userCredential = await signInWithCustomToken(auth, input.token)
-      console.log('userCredential', userCredential)
-      const user = userCredential.user
-      console.log(user)
+      // check valid token
+
       result = {
         displayName: user.displayName,
         email: user.email
